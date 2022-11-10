58541f141b236afc0be50b2d755083bd4ea1c8f2
elrufo21 <neburtnt@gmail.com>
2022-11-09 19:09:31 -0500

    Funciones agragadas al modelo

--- controller_func/automatizar_crud/crear_modelclass/accion.php
+++ controller_func/automatizar_crud/crear_modelclass/accion.php
@@ -11,10 +11,13 @@ class crear_class{
        
         
         fwrite($archivo, "<?php\r\n include_once('cn.php'); \r\n ");
+        
         fwrite($archivo, "class $this->bd extends cn{\r\n");
         for ($i=1; $i <= $this->bd_campos ; $i++) { 
             fwrite($archivo,"var $"."val$i;\r\n");
         }
+
+        //Funcion crear
         fwrite($archivo, "function create(){\r\n");
         fwrite($archivo, '$'.'query="INSERT INTO '. "$this->bd" .' VALUES(0,');
         for($i=2; $i <= $this->bd_campos-4 ; $i++){
@@ -28,6 +31,28 @@ class crear_class{
         fwrite($archivo, ' mysqli_close($this->f_cn());'."\r\n");
         fwrite($archivo, ' return $rs;'."\r\n");
         fwrite($archivo, "} \r\n");
+        //Funcion leer
+        fwrite($archivo, "function read(){\r\n");
+        fwrite($archivo,'$query="SELECT * FROM '."$this->bd".'";'."\r\n");
+        fwrite($archivo, '$rs = mysqli_query($this->f_cn(), $query);'."\r\n");
+        fwrite($archivo, 'mysqli_close($this->f_cn());'."\r\n");
+        fwrite($archivo, 'return $rs;'."\r\n");
+        fwrite($archivo, "} \r\n");
+        //funcion update
+        fwrite($archivo, "function update(){\r\n");
+        fwrite($archivo,'$query="UPDATE '."$this->bd".' SET fechaactualiza=now(),id_usuarioactualiza=1 WHERE id_'."$this->bd = '".'$this->id_'."$this->bd'".'"'.";\r\n");
+        fwrite($archivo,'$rs = mysqli_query($this->f_cn(), $query);'."\r\n");
+        fwrite($archivo,'mysqli_close($this->f_cn());'."\r\n");
+        fwrite($archivo,'return $rs;'."\r\n");
+        fwrite($archivo, "} \r\n");
+
+        fwrite($archivo, 'function estado($estado){'."\r\n");
+        fwrite($archivo,'$query="UPDATE '."$this->bd"." SET estado ='".'$estado'."' WHERE id_$this->bd = "."'".'$this->id_'."$this->bd'".'";'."\r\n");
+        fwrite($archivo, '$rs = mysqli_query($this->f_cn(), $query);'."\r\n");
+        fwrite($archivo,'mysqli_close($this->f_cn());'."\r\n");
+        fwrite($archivo,'return $rs;'."\r\n");
+        fwrite($archivo, "} \r\n");
+        
         fwrite($archivo, "} ?>\r\n");
         fflush($archivo);
 
--- model_class/aaa.php
+++ model_class/aaa.php
@@ -1,6 +1,6 @@
 <?php
  include_once('cn.php'); 
- class hola_mundo extends cn{
+ class aaa extends cn{
 var $val1;
 var $val2;
 var $val3;
@@ -14,22 +14,25 @@ var $val10;
 var $val11;
 var $val12;
 function create(){
-$query="INSERT INTO hola_mundo VALUES(0,'$this->val2','$this->val3','$this->val4','$this->val5','$this->val6','$this->val7','$this->val8',now(),now(),1,1)";
+$query="INSERT INTO aaa VALUES(0,'$this->val2','$this->val3','$this->val4','$this->val5','$this->val6','$this->val7','$this->val8',now(),now(),1,1)";
  $rs=mysqli_query($this->f_cn(),$query);
  mysqli_close($this->f_cn());
  return $rs;
 } 
 function read(){
-$query="SELECT * FROM hola_mundo";
+$query="SELECT * FROM aaa";
 $rs = mysqli_query($this->f_cn(), $query);
 mysqli_close($this->f_cn());
 return $rs;
 } 
 function update(){
-$query="UPDATE hola_mundo SET fechaactualiza=now(),id_usuarioactualiza=1 WHERE id_hola_mundo = '$this->id_hola_mundo'";
+$query="UPDATE aaa SET fechaactualiza=now(),id_usuarioactualiza=1 WHERE id_aaa = '$this->id_aaa'";
 $rs = mysqli_query($this->f_cn(), $query);
 mysqli_close($this->f_cn());
 return $rs;
 } 
+function estado($estado){
+$query="UPDATE aaa SET estado ='$estado' WHERE id_aaa = $this->id_";
+ 
 } 
-?>
+} ?>
